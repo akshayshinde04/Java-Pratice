3ad5f40b1b6f8513cbb10adb50487c9b6431743f
unknown <akshayHVshinde04@gmail.com>
2021-09-22 11:57:19 +0530

    Added: Addition

---
+++ src/test/java/bridgelabz/javapratice/Addition.java
@@ -0,0 +1,13 @@
+package bridgelabz.javapratice;
+
+public class Addition {
+	public static void main(String[] args) {
+		int n1 = 20 ;
+		int n2 = 30 ;
+		int sum = n1 + n2 ;
+		
+		System.out.println("Sum of the 2 numbers "+sum);
+		
+	}
+
+}
